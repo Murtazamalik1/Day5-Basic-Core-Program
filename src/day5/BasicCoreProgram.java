091ea176cfffbae305e88244bd07634352a3094b
murtazamalik <murtazamalik1319@gmail.com>
2023-02-28 12:27:49 +0530

    Adding Harmonic Number

--- src/day5/BasicCoreProgram.java
+++ src/day5/BasicCoreProgram.java
@@ -1,20 +1,22 @@
 package day5;
 
-
 import java.util.Scanner;
 
 public class BasicCoreProgram {
     public static void main(String[] args) {
-        int year;
+        double n;
+        double sum = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter any year to check its leap year or not");
-        year = scanner.nextInt();
+        System.out.println("Enter Harmonic Number");
+        n = scanner.nextInt();
 
-        if (year %2 == 0 && year%4 == 0 && year%100 != 0 || year%400 == 0 ){
-            System.out.println(" Leap Year :" + year);
-        }
-        else {
-            System.out.println("Not Leap Year :" + year );
+        if (n > 0) {
+            for (double i = 0; i <= n; i++) {
+                sum = n + (1 / i);
+            }
+            System.out.println(sum);
+        } else {
+            System.out.println("enter  the positive number");
         }
     }
 }
